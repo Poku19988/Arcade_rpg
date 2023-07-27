d09761ce1d4f2837fbd1743f183e23d52dc75691
Poku19988 <127587700+Poku19988@users.noreply.github.com>
2023-07-27 18:57:07 +0330

    Add files via upload

---
+++ overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="overworld" tilewidth="16" tileheight="16" tilecount="152" columns="19">
+ <image source="../../../../Videos/Free Pixel Art overworld tileset/PNG/overworld type 2.png" width="304" height="128"/>
+</tileset>
